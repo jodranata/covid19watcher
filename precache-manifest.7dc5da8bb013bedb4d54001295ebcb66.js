bbbd309ebbb97170c1ce36188d300df2a840c080
Jodi Pranata <jodranata@gmail.com>
2020-05-20 22:45:37 +0700

    Updates

---
+++ precache-manifest.7dc5da8bb013bedb4d54001295ebcb66.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c3097557eee7cb1633cfed26f58cfe",
+    "url": "/covid19watcher/index.html"
+  },
+  {
+    "revision": "811b7373b85d5356c8ea",
+    "url": "/covid19watcher/static/css/main.c0244e16.chunk.css"
+  },
+  {
+    "revision": "70a007e205c5cbfdb750",
+    "url": "/covid19watcher/static/js/2.d9fd21f9.chunk.js"
+  },
+  {
+    "revision": "d14f21e42c09509e44bd25febcf4b2dc",
+    "url": "/covid19watcher/static/js/2.d9fd21f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811b7373b85d5356c8ea",
+    "url": "/covid19watcher/static/js/main.aef33dbe.chunk.js"
+  },
+  {
+    "revision": "66c56f785bf6d335a8ec",
+    "url": "/covid19watcher/static/js/runtime-main.8c48be40.js"
+  },
+  {
+    "revision": "d73147685a4bd24a1ae9f02e3e90d93e",
+    "url": "/covid19watcher/static/media/prevention1.d7314768.jpg"
+  },
+  {
+    "revision": "5a37e217789b1629f218521224667761",
+    "url": "/covid19watcher/static/media/prevention2.5a37e217.png"
+  },
+  {
+    "revision": "d464c3921abaeba73bda80a84c181a52",
+    "url": "/covid19watcher/static/media/prevention3.d464c392.png"
+  },
+  {
+    "revision": "0573af75574ab9ea64faae543e7e4ddb",
+    "url": "/covid19watcher/static/media/prevention4.0573af75.png"
+  },
+  {
+    "revision": "59ba5123de58adf6a3f21358e6e0304e",
+    "url": "/covid19watcher/static/media/prevention5.59ba5123.png"
+  },
+  {
+    "revision": "ca8ec1a79d9347ea32031d6813a4eb41",
+    "url": "/covid19watcher/static/media/prevention6.ca8ec1a7.png"
+  }
+]);
